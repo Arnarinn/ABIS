74376d2c7c25e6bff33881faa5381dea5ad0f2a7
Arnar Geirsson <arnarinn105@gmail.com>
2018-11-27 17:06:10 +0000

    seprated backend and frontent

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-class startup:
-    car_pure = open('cars')
-    car_info = car_pure.read().split('\n')
-    for ci in car_info:
-        car = ci.split(' ')
-        print('Type: ' + car[0])
-        print('Color: ' + car[1])
-        print('Price: ' + car[2])
-        print('_________________')
+
+def carinit():
+    # Makes an arrays of the lines taken from the cars.txt file
+    cars_whole = open('data/cars.txt').read().split('\n')
+    # Gets the amount of cars in the file
+    car_amount = len(cars_whole)
+    # Gets the amount of variables a car has
+    car_variables = cars_whole[0].count(' ')+1
+    # Makes an empty car_variable x car_amount matrix of arrays.
+    cars_matrix = [[0] * car_variables for i in range(car_amount)]
+    # Inserts the values from the file into the array
+    i: int = 0
+    # Further splits each car line into its variables and inputs it into the empty matrix
+    for car in cars_whole:
+        c = car.split(' ')
+        cars_matrix[i] = c
+        i += 1
+    return cars_matrix
+
+def customerinit():
+    # Makes an arrays of the lines taken from the customers.txt file
+    customers_whole = open('data/customers.txt').read().split('\n')
+    # gets the amount of customers in the file
+    customer_amount = len(customers_whole)
+    # Gets the amount of variables a customer has
+    customer_variables = customers_whole[0].count(' ')+1
+    # Makes an empty customer_variables X customer_amount matrix of arrays
+    customers_matrix = [[0] * customer_variables for i in range(customer_amount)]
+    i: int = 0
+    # Further splits each customer line into its variables and inputs it into the empty matrix
+    for customer in customers_whole:
+        c = customer.split(' ')
+        customers_matrix[i] = c
+        i += 1
+    return customers_matrix
