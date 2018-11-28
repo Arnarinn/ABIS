84f59dc9ef165c8675e2d8814a6e74dc98ad1c11
Arnar Geirsson <arnarinn105@gmail.com>
2018-11-28 14:01:36 +0000

    added file write

--- data.py
+++ data.py
@@ -1,35 +1,51 @@
+class getData:
+    def __init__(self):
+        # Get the data from the car csv file
+        self.carsWholeR = open('data/cars.csv')
+        # Gets the data from the customer csv file
+        self.customersWholeR = open('data/customers.csv')
+        self.customersWholeA = open('data/customers.csv', 'a')
 
-def carinit():
-    # Makes an arrays of the lines taken from the cars.txt file
-    cars_whole = open('data/cars.txt').read().split('\n')
-    # Gets the amount of cars in the file
-    car_amount = len(cars_whole)
-    # Gets the amount of variables a car has
-    car_variables = cars_whole[0].count(' ')+1
-    # Makes an empty car_variable x car_amount matrix of arrays.
-    cars_matrix = [[0] * car_variables for i in range(car_amount)]
-    # Inserts the values from the file into the array
-    i: int = 0
-    # Further splits each car line into its variables and inputs it into the empty matrix
-    for car in cars_whole:
-        c = car.split(',')
-        cars_matrix[i] = c
-        i += 1
-    return cars_matrix
+    def carInit(self):
+        # Makes an arrays of the lines taken from the cars.csv file
+        with self.carsWholeR as c:
+            carsWhole = c.readlines()
+        # Gets the amount of cars in the file
+        carAmount = len(carsWhole)
+        # Gets the amount of variables a car has
+        carVariables = carsWhole[0].count(' ')+1
+        # Makes an empty car_variable x car_amount matrix of arrays.
+        cars_matrix = [[0] * carVariables for i in range(carAmount)]
+        # Inserts the values from the file into the array
+        i: int = 0
+        # Further splits each car line into its variables and inputs it into the empty matrix
+        for car in carsWhole:
+            c = car.split(',')
+            cars_matrix[i] = c
+            i += 1
+        self.carsWholeR.close()
+        return cars_matrix
 
-def customerinit():
-    # Makes an arrays of the lines taken from the customers.txt file
-    customers_whole = open('data/customers.txt').read().split('\n')
-    # gets the amount of customers in the file
-    customer_amount = len(customers_whole)
-    # Gets the amount of variables a customer has
-    customer_variables = customers_whole[0].count(' ')+1
-    # Makes an empty customer_variables X customer_amount matrix of arrays
-    customers_matrix = [[0] * customer_variables for i in range(customer_amount)]
-    i: int = 0
-    # Further splits each customer line into its variables and inputs it into the empty matrix
-    for customer in customers_whole:
-        c = customer.split(',')
-        customers_matrix[i] = c
-        i += 1
-    return customers_matrix
+    def customerInit(self):
+        # Makes an arrays of the lines taken from the customers.csv file
+        with self.customersWholeR as c:
+            customersWhole = c.readlines()
+        # gets the amount of customers in the file
+        customer_amount = len(customersWhole)
+        # Gets the amount of variables a customer has
+        customer_variables = customersWhole[0].count(' ')+1
+        # Makes an empty customer_variables X customer_amount matrix of arrays
+        customers_matrix = [[0] * customer_variables for i in range(customer_amount)]
+        i: int = 0
+        # Further splits each customer line into its variables and inputs it into the empty matrix
+        for customer in customersWhole:
+            c = customer.split(',')
+            customers_matrix[i] = c
+            i += 1
+        self.customersWholeR.close()
+        return customers_matrix
+
+    def customerInsert(self, name, age):
+        str = '\n'+name+','+age
+        self.customersWholeA.write(str)
+        self.customersWholeA.close()
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import data
+from data import getData
+
 
 class UI:
+    data = getData()
     print('Welcome to the ABIS manager program!')
     print('___________________________________')
-    print('1. New order'
-          '2. View order'
-          '3. View cars'
-          '4. New customer'
-          '5. View customer')
-    #option = input('Choose')
-    carMatrix = data.carinit()
-    print(carMatrix)
-    customerMatrix = data.customerinit()
-    print(customerMatrix)
+    print('1. New order \n'
+          '2. View order \n'
+          '3. View cars \n'
+          '4. New customer \n'
+          '5. View customer \n')
+    option = input('Choose')
+    if option == 3:
+        carData = data.carInit()
+        for v in carData:
+            print('Type: ' + v[0] + '\n'
+                  'Color: ' + v[1] + '\n'
+                  'Price: ' + v[2])
