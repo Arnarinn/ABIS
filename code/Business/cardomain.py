5e6fc78c8ef0f02f14f13d434962627beacd1370
Sigurgeir <sigurgeir16@ru.is>
2018-12-06 13:32:54 +0000

    Show all available cars and show all rented out cars

--- code/Business/cardomain.py
+++ code/Business/cardomain.py
@@ -1,6 +1,8 @@
 from Repositories.getcars import GetCars
 
+
 class CarDomain:
+
 
     def __init__(self):
         cars = GetCars()
@@ -10,6 +12,7 @@ class CarDomain:
     def __str__(self):
         return '{}'.format(self.__carList)
 
+
     # Returns a list of all available cars
     def availableCars(self):
         aCars = []
@@ -17,12 +20,14 @@ class CarDomain:
             if x.getStatus() == '0':
                 aCars.append(x)
         return aCars
-    
+
+
     # Made for testing
     def setAllAsUnavailable(self):
         aCars = self.availableCars()
         for x in aCars:
             x.setStatus('1')
+
 
     # Returns a list of all unavailable cars
     def unavailableCars(self):
@@ -32,25 +37,29 @@ class CarDomain:
                 uACars.append(x)
         return uACars
 
-    # THE NEXT 3 FUNCTIONS MIGHT BELONG TO THE UI LAYER
-    # Prints a table with 
-    def printTable(self, myCarList):
-        print('--------------------------------------------------')
-        print('%-15s%-15s%-10s%-10s' % ('|' + 'Type', '|' + 'Manufacturer', \
-            '|' + 'Plate Nr', '|' + 'Price   ' + '|'))
-        print('==================================================')
-        for x in myCarList:
-            print('%-15s%-15s%-10s%-10s' % ('|' + x.getType(),\
-                 '|' + x.getManufacturer(),\ 
-                 '|' + x.getPlate(), '|' + x.getCost() + '|'))
-            print('--------------------------------------------------')
-    
-    # Displays information about available cars relevant to customers
-    def dispAvailableCars(self):
-        listOfAvailables = self.availableCars()
-        self.printTable(listOfAvailables)
 
-    # Displays information about unavailable cars relevant to customers
-    def dispUnavailableCars(self):
-        listOfUnavailables = self.unavailableCars()
-        self.printTable(listOfUnavailables)
+    # THE NEXT 3 FUNCTIONS MIGHT BELONG TO THE UI LAYER
+    ##################################################################################
+    # Prints a table with                                                            #
+    def printTable(self, myCarList):                                                 #
+        print('--------------------------------------------------')                  #
+        print('%-15s%-15s%-10s%-10s' % ('|' + 'Type', '|' + 'Manufacturer', \        
+            '|' + 'Plate Nr', '|' + 'Price   ' + '|'))                               #
+        print('==================================================')                  #
+        for x in myCarList:                                                          #
+            print('%-15s%-15s%-10s%-10s' % ('|' + x.getType(),\                      
+                 '|' + x.getManufacturer(),\ 
+                 '|' + x.getPlate(), '|' + x.getCost() + '|'))                       #
+            print('--------------------------------------------------')              #
+                                                                                     #
+    # Displays information about available cars relevant to customers                #
+    def dispAvailableCars(self):                                                     #
+        listOfAvailables = self.availableCars()                                      #
+        self.printTable(listOfAvailables)                                            #
+                                                                                     #
+    # Displays information about unavailable cars relevant to customers              #
+    def dispUnavailableCars(self):                                                   #
+        listOfUnavailables = self.unavailableCars()                                  #
+        self.printTable(listOfUnavailables)                                          #
+    ##################################################################################
+
