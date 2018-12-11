2db4940a33ceef46fe838db70822deed0b480433
skjaldbaka <gissurb18@ru.is>
2018-12-11 14:30:06 +0000

    Did the CM table but it needs adjusments

--- code/UI/carui.py
+++ code/UI/carui.py
@@ -8,7 +8,7 @@ class CarUi:
 
 
     #Prints a table with the contents of myCarList
-    def printTable(self, myCarList):                                                 
+    def printCSTable(self, myCarList):                                                 
         print('--------------------------------------------------')
         #Table has 4 colums with size: 15, 15, 10, 10 respectively
         # columns are filled with strings
@@ -22,7 +22,25 @@ class CarUi:
                  '|' + x.getPlate(), '|' + x.getCost() + '|'))                       
             print('--------------------------------------------------')
 
-
+     def printCMTable(self, myCarList):                                                 
+        print('--------------------------------------------------')
+        #Table has 4 colums with size: 15, 15, 10, 10 respectively
+        # columns are filled with strings
+        # columns are aligned to the left                  
+        print('%-15s%-15s%-15s%-15s%-15s-15s%-15s%-10s%-10s%-10s-15s%-10s-10s%' % ('|' + 'Type', '|' + 'Manufacturer', \
+            '|' + 'Color', '|' + 'Distance', '|' + 'Year', '|' + 'Plate Nr', \ 
+            '|' + 'Number of seats', '|' + 'Number of doors', \ 
+            '|' + 'Inspected', '|' + 'Fuel', '|' + 'Wheel drive', \
+            '|' + 'Shifting option', '|' + 'Status', '|' + 'Price ' + '|'))                               
+        print('==================================================')                  
+        for x in myCarList:                                                          
+            print('%-15s%-15s%-15s%-15s%-15s-15s%-15s%-10s%-10s%-10s-15s%-10s-10s%' % ('|' + x.getType(),\
+                '|' + x.getManufacturer(),\
+                '|' + x.getColor(), '|' + x.getDistance(), '|' + x.getYear(), \ 
+                '|' + x.getPlate(),  '|' + x.getSeats(), '|' + x.getDoors(), \
+                '|' + x.getInspected(), '|' + x.getFuel(), '|' + x.getWheel(), \
+                '|' + x.getShifting(), '|' + x.getSatus(), '|' + x.getCost() + '|'))                       
+            print('--------------------------------------------------')
     #Calls the printTable function on all available cars
     def dispAvailableCars(self):                                                                                        
         self.printTable(self.__dom.availableCars()) 
