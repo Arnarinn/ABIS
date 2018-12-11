f189007ecec446bf27c005722b82daaf03c05024
Sigurgeir <sigurgeir16@ru.is>
2018-12-11 16:39:24 +0000

    Create New Order finished

---
+++ code/Models/cartype.py
@@ -0,0 +1,20 @@
+
+
+class CarType:
+
+    def __init__(self):
+        self.__sedan = 12000
+        self.__sport = 19000
+        self.__jeep = 33000
+
+
+    def getSedanPrice(self):
+        return self.__sedan
+
+    
+    def getSportPrice(self):
+        return self.__sport
+
+
+    def getJeepPrice(self):
+        return self.__jeep
